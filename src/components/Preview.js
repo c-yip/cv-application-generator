ca7ad92f19fd7264db8ed73db7cb103f45e13117
Christian <christianyip92@gmail.com>
2022-09-28 16:43:15 -0700

    Add objective preview elements.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -11,17 +11,23 @@ export default function Preview(props) {
   
   return (
     <div className="preview">
-      <div className="name">
-        <h1>{props.data.firstName} {props.data.middle} {props.data.lastName}</h1>
+      
+      <div className="peronal-info">
+        <div className="name">
+          <h1>{props.data.firstName} {props.data.middle} {props.data.lastName}</h1>
+        </div>
+        <div className="contact">
+          <p>{props.data.location}</p>
+          <p>{props.data.email}</p>
+          <p>{formatPhoneNumber(props.data.phone)}</p>
+          <p>{props.data.linkedIn}</p>
+        </div>
       </div>
 
-      <div className="contact">
-        <p>{props.data.location}</p>
-        <p>{props.data.email}</p>
-        <p>{formatPhoneNumber(props.data.phone)}</p>
-        <p>{props.data.linkedIn}</p>
+      <div className="objective">
+        <h2 className="objective-title">Objective</h2>
+        <p className="objective-info">{props.data.objective}</p>
       </div>
-
     </div>
   )
 }
