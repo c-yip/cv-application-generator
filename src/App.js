d1ad2ecde8dbc3f8972ec01e975e5149bcc899d6
Christian <christianyip92@gmail.com>
2022-09-28 12:23:50 -0700

    Add inputs for personal information.
    Add input attributes with applicable props and event listeners.
    
    Add state object and handle input function.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Input from './components/Input';
 import Preview from './components/Preview';
 import Footer from './components/Footer';
 
 function App() {
+  const [data, setData] = React.useState({
+    firstName: '', middle: '', lastName: '', phone: '', email: ''
+  });
+  
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+    console.log(data);
+  }
+
   return (
     <div className="App">
       <Header />
-      <Input />
+      <Input 
+        handleChange={handleChange}
+        data={data}
+      />
       <Preview />
       <Footer />
     </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,59 @@
 export default function Input(props) {
   return (
-    <p>Test</p>
+    <form>
+      <legend>Personal Information</legend>
+
+      <label htmlFor="input">First Name</label>
+      <input 
+        type="text"
+        placeholder="First Name"
+        className="input"
+        name="firstName"
+        onChange={props.handleChange}
+        value={props.data.firstName}
+      />
+
+      <label htmlFor="input">Middle Name or Initial</label>
+      <input 
+        type="text"
+        placeholder="Middle"
+        className="input"
+        name="middle"
+        onChange={props.handleChange}
+        value={props.data.middle}
+      />
+
+      <label htmlFor="input">Last Name</label>
+      <input 
+        type="text"
+        placeholder="Last Name"
+        className="input"
+        name="lastName"
+        onChange={props.handleChange}
+        value={props.data.lastName}
+      />
+
+      <label htmlFor="input">Phone Number</label>
+      <input 
+        type="tel"
+        placeholder="Phone Number"
+        className="input"
+        name="phone"
+        onChange={props.handleChange}
+        value={props.data.phone}
+      />
+
+      <label htmlFor="input">Email Address</label>
+      <input 
+        type="email"
+        placeholder="Email Address"
+        className="input"
+        name="email"
+        onChange={props.handleChange}
+        value={props.data.email}
+      />
+
+      <button>Submit</button>
+    </form>
   )
 }
